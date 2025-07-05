1c4b6e69741942c1ddf2803ecee36c57b389ef4c
emerityi <emjor08@gmail.com>
2025-07-05 10:01:23 -0500

    Add files via upload

---
+++ audiobutton.js
@@ -0,0 +1,16 @@
+
+let modemSound
+let playButton
+
+
+function preload(){
+	modemSound = loadSound('ModemSound.mp3')
+}
+
+
+function setup(){
+	createCanvas(windowWidth,windowHeight)
+	playButton = createButton('Play Audio')
+
+
+}
--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
 let furby
-
+let gary
 function preload(){
   furby = loadImage('furby.png')
+
+  gary = loadImage('gary.gif')
 }
+
 
 function setup() {
   createCanvas(600,600)
@@ -10,20 +13,34 @@ function setup() {
 
 function draw() {
   background(220);
-  
-  fill(114, 118, 247)
-  rect(0,430,700,400)
+
   stroke(204, 186, 153)
+  if(mouseIsPressed == true){
+    fill(35, 55, 61)
+  } else{
+
+  fill(114, 118, 247)
+}
+  rect(0,430,700,400)
   
   stroke(178,221,237)
   fill(204, 186, 153)
   circle(300,360,295)
   
   stroke(178,221,237)
+  if(mouseIsPressed == true){
+    fill(12,22,97)
+  }else{
+
   fill(178, 221, 237)
+  }
   rect(0,0,700,430)
   
+  if(mouseIsPressed == true){
+    fill(150)
+  }else{
   fill(243, 255, 79)
+  }
   circle(580,30,200)
   
   stroke( 251, 252, 250)
@@ -45,7 +62,11 @@ circle(270,100,70)
   
   stroke(145, 237, 52)
   fill(145, 237, 52)
-  triangle(450,200,450,100,540,150)
-  image(furby, 200, 200)
+  triangle(350,300,350,200,440,245)
 
+  stroke(0,0,0)
+  strokeWeight(2)
+  line(350,430,350,200)
+  //image(furby, 200, 200)
+  image(gary, 100,60, 100, 100)
 }
