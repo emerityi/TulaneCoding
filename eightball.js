547812f4ba72a2d1ae5a0dcd43515186c7c66ed3
emerityi <emjor08@gmail.com>
2025-07-08 11:08:07 -0500

    Add files via upload

--- eightball.js
+++ eightball.js
@@ -1,12 +1,24 @@
+let eightball;
+let showImage = true;
+
 function preload(){
   eightball =loadImage('Magic_eight_ball.png')
 }
 
 function setup(){
   createCanvas (windowWidth, windowHeight)
+  eightball
 }
 
 function draw(){
   background(100)
+  if (showImage){
   image(eightball,550,200,400,400)
+  }
+}
+
+function keyPressed(){
+  if (keyCode===ENTER){
+    showImage = false;
+  }
 }
