692a145ae763a0e82aa5cce6b76f7b7550fef407
emerityi <emjor08@gmail.com>
2025-07-11 12:39:14 -0500

    Add files via upload

--- eightball.js
+++ eightball.js
@@ -1,10 +1,14 @@
-let randomTexts = ["Wishful Thinking!","Keep Hoping!","Not In A Million Years!","Without A Doubt","Can You Handle The Truth?","Definitely Maybe","Try Google","Eight Ball.exe Has Stopped Working","Bop it, Twist It, Pull It", "Of Course Queen!","HELP! I'M STUCK IN AN EIGHT BALL"]
+let randomTexts = ["Wishful Thinking!","Keep Hoping!","Not In A Million Years!","Without A Doubt","Can You Handle The Truth?","Definitely Maybe","Try Google"]
 let currentText = " "
 let myInput;
 let eightball;
 let imageAlpha =255;
 let isFading =false;
 let showImage = true;
+let refreshButton
+
+let eightballBool = true
+let scene2Bool = false
 
 function preload(){
   eightball =loadImage('Magic_eight_ball.png')
@@ -18,18 +22,51 @@ function setup(){
     myInput.position(580, 50);
   myInput.size(400);
   myInput.changed(answer)
- 
+  refreshButton = createButton('Refresh')
+refreshButton.position(windowWidth/2, windowHeight/2 + 200)
+refreshButton.mousePressed(refreshApp)
+refreshButton.hide()
+
+
 }
+
+function refreshApp(){
+   eightballBool = false
+   scene2Bool = true
+  background(200, 50, 200)
+
+
+  imageAlpha =255;
+  myInput.value('')
+  currentText = " "
+    isFading =false;
+
+  refreshButton.hide()
+
+}
+ 
 
 function answer(){
   if(myInput.value() != ''){
    isFading =true;
    currentText = random(randomTexts);
  }
+ refreshButton.show()
 }
 
 function draw(){
-  background(150)
+  if(eightballBool == true){
+    eightballScene()
+  }
+  
+ if(scene2Bool == true){
+    refreshApp()
+  }
+}
+
+
+function eightballScene(){
+   background(150)
   tint(255,imageAlpha);
   textSize(24);
   text(currentText, width/2, height/2);
